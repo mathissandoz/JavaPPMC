cc565dc7c76c32d7e8c32ab8a5d2becbe3d23b78
Mathis Sandoz <45627872+mathissandoz@users.noreply.github.com>
2018-12-07 23:21:46 +0100

    Update PPMC.java

--- PPMC.java
+++ PPMC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PPMC {
+public class PpmcDiv1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,8 +8,22 @@ public class PPMC {
 		boolean pp2 = false;
 		int mppp2;
 		int mpp3;
-		boolean pp3;
+		boolean pp3 = false;
 		int mpp2;
+		boolean pp4 = false;
+		int mpp4;
+		boolean pp5 = false;
+		int mpp5;
+		boolean pp6 = false;
+		int mpp6;
+		boolean pp7 = false;
+		int mpp7;
+		boolean pp8 = false;
+		int mpp8;
+		boolean pp9 = false;
+		int mpp9;
+		
+		
 		
 		Scanner sc = new Scanner(System.in);
 				int i = sc.nextInt();
@@ -30,9 +44,56 @@ public class PPMC {
 				mpp3 = i % 3;
 				if (mpp3 == 0)
 					pp3 = true;
+				else
+				{
+					mpp4 = i % 4;
+					if (mpp4 == 0)
+						pp4 = true;
 				
-			}
-		}
+					else
+				{
+						mpp5 = i % 5;
+						if (mpp5 == 0)
+							pp5 = true;
+						else
+						{
+							mpp6 = i % 6;
+						if (mpp6 == 0)
+							pp6 = true;
+							else
+						{
+								mpp7 = i % 7;
+								if (mpp7 == 0)
+									pp7 = true;
+								else
+								{
+									mpp8 = i % 8;
+								if (mpp8 == 0)
+									pp8 = true;
+									else
+									{
+										mpp9 = i % 8;
+									if (mpp9 == 0)
+										pp9 = true;
+									}
+								}	
+							}	
+						}							
+					}							
+				}								
+			}								
+		}	
 	}
-
+	{
+	if (pp2 = true);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
 }
